aa50ceae60a306f832f9c1ef74817584a8f2a52d
jonas <jonas@halfdanj.dk>
2009-11-19 13:53:44 +0000

    reorganize

---
+++ fub001/src/Plugins/PluginIncludes.h
@@ -0,0 +1,12 @@
+//
+//  PluginIncludes.h
+//  openFrameworks
+//
+//  Created by Jonas Jongejan on 15/11/09.
+//  Copyright 2009 HalfdanJ. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#include "_ExampleOutput.h"
+//#include "_ExampleOutputAgain.h"
