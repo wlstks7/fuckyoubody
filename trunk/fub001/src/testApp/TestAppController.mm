af0f034ae9e2b96124cf7fdc9663b202d1e4ceda
jonas@halfdanj.dk <jonas@halfdanj.dk@e8aa6d0e-c866-11de-ba9a-9db95b2bc6c5>
2009-11-19 13:42:58 +0000

    reorganize some stuff

---
+++ trunk/fub001/src/testApp/TestAppController.mm
@@ -0,0 +1,41 @@
+//
+//  TestAppController.m
+//
+//  Created by Jonas Jongejan on 03/11/09.
+//
+
+#import "TestAppController.h"
+#include "PluginIncludes.h"
+#include "testApp.h"
+#include "ofAppCocoaWindow.h"
+
+extern testApp * OFSAptr;
+extern ofAppBaseWindow * window;
+
+@implementation TestAppController
+
+
+-(void) awakeFromNib {
+	NSLog(@"Awake from nib");
+	[pluginManagerController addHeader:@"Test"];
+	[pluginManagerController addPlugin:[[_ExampleOutput alloc] init]];
+	[pluginManagerController addHeader:@"Test2"];
+//	[pluginManagerController addPlugin:[[_ExampleOutputAgain alloc] init]];
+
+	
+	[pluginManagerController setFrame:[mainView bounds]];
+	[mainView addSubview:pluginManagerController];
+	
+	baseApp = OFSAptr;
+	cocoaWindow = window;
+	
+	((ofAppCocoaWindow*)cocoaWindow)->windowController = self;
+	
+	((ofAppCocoaWindow*)cocoaWindow)->setup();
+
+	[pluginManagerController callSetup];
+	
+	
+}
+
+@end
