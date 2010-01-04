08440c2847e7d4a711d8f2611a79b03234636a04
ole <ole@kristensen.name>
2010-01-04 15:41:26 +0000

    Jule ferie

---
+++ fub001/src/Plugins/outputs/Ulykke/Ulykke.mm
@@ -0,0 +1,58 @@
+//
+//  Ulykke.m
+//  openFrameworks
+//
+//  Created by Fuck You Buddy on 19/12/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "PluginIncludes.h"
+
+
+@implementation Ulykke
+-(void) draw:(CFTimeInterval)timeInterval displayTime:(const CVTimeStamp *)outputTime{
+	vector<ofxPoint2f> points;
+	Blob *b;
+	[GetPlugin(ProjectionSurfaces) apply:"Front" surface:"Floor"];
+
+	for(b in [tracker(1) blobs]){
+		glBegin(GL_LINE_STRIP);
+		for (int i=0; i<[b nPts]; i++) {
+			ofxPoint2f p = [GetPlugin(ProjectionSurfaces) convertFromProjection:[b pts][i]  surface:Surf("Front","Floor")];
+			glVertex2f(p.x, p.y);
+		}
+		glEnd();
+
+		ofBeginShape();
+			for (int i=0; i<[b nPts]; i++) {
+			ofxPoint2f p = [GetPlugin(ProjectionSurfaces) convertFromProjection:[b pts][i]  surface:Surf("Front","Floor")];
+			ofVertex(p.x, p.y);
+		}
+		ofEndShape(true);
+		
+	}
+	
+	glPopMatrix();
+	
+	[GetPlugin(ProjectionSurfaces) apply:"Back" surface:"Floor"];
+	
+	for(b in [tracker(1) blobs]){
+		glBegin(GL_LINE_STRIP);
+		for (int i=0; i<[b nPts]; i++) {
+			ofxPoint2f p = [GetPlugin(ProjectionSurfaces) convertFromProjection:[b pts][i]  surface:Surf("Front","Floor")];
+			glVertex2f(p.x, p.y);
+		}
+		glEnd();
+
+		ofBeginShape();
+		for (int i=0; i<[b nPts]; i++) {
+			ofxPoint2f p = [GetPlugin(ProjectionSurfaces) convertFromProjection:[b pts][i]  surface:Surf("Front","Floor")];
+			ofVertex(p.x, p.y);
+		}
+		ofEndShape(true);
+	}
+	
+	glPopMatrix();
+	
+}
+@end
