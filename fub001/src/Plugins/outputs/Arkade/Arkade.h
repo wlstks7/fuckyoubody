13fc173b5d13e00c2e14e8f73c5e191f13695930
jonas <jonas@halfdanj.dk>
2010-01-21 17:17:43 +0000

    mouse event

--- fub001/src/Plugins/outputs/Arkade/Arkade.h
+++ fub001/src/Plugins/outputs/Arkade/Arkade.h
@@ -5,10 +5,18 @@
 #import <Cocoa/Cocoa.h>
 #include "Plugin.h"
 #include "ofMain.h"
+#define FLOORGRIDSIZE 8
 
 @interface Arkade : ofPlugin {
-	ofVideoPlayer * wall;
-		ofVideoPlayer * floor;
+	IBOutlet NSButton * floorSquaresButton;
+	float floorSquaresOpacity[ FLOORGRIDSIZE * FLOORGRIDSIZE ];
+	
+	vector<ofxPoint2f> cookies;
+	
+	ofxPoint2f * pacmanPosition;
+	ofxVec2f * pacmanDir;
 }
+
+-(int) getIatX:(float)x Y:(float)y;
 
 @end
