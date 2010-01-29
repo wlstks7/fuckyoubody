c17cd729a88791de451d0e8030938957e81f34a0
jonas@halfdanj.dk <jonas@halfdanj.dk@e8aa6d0e-c866-11de-ba9a-9db95b2bc6c5>
2010-01-29 09:35:12 +0000

    Send qlab cues with alt click on custom controllers

--- trunk/fub001/src/Plugins/outputs/DMXOutput/DMXOutput.h
+++ trunk/fub001/src/Plugins/outputs/DMXOutput/DMXOutput.h
@@ -7,7 +7,9 @@
 #include "ofMain.h"
 #include "ofxVectorMath.h"
 
-
+#define BLENDING_OVER 0
+#define BLENDING_ADD 1
+#define BLENDING_HIGHEST 2
 
 @interface Lamp : NSObject
 {
@@ -33,6 +35,7 @@
 }
 
 -(void) setLamp:(float)_r g:(float)_g b:(float)_b a:(float)_a;
+-(NSColor*) getColor;
 @end
 
 
@@ -46,17 +49,48 @@
 -(void) setLamp:(float)_v;
 @end
 
+@interface DiodeBox : NSObject
+{
+	NSArray * lamps;
+	int startAddress;
+	
+}
+@property (assign, readwrite) NSArray * lamps;
+
+-(id) initWithStartaddress:(int) address;
+-(void) addColor:(NSColor*)color onLamp:(ofPoint)lamp withBlending:(int)blending;
+-(LedLamp*) getLampAtPoint:(ofPoint)point;
+-(void) reset;
+@end
+
+
 
 
 @interface DMXOutput : ofPlugin {
 	NSThread * thread;
-	NSMutableArray * lamps;
+	NSMutableArray * diodeboxes;
 	ofSerial * serial;
 	bool ok;
 	bool connected;
 	
 	pthread_mutex_t mutex;
-
+	
+	IBOutlet NSColorWell * backgroundColor;
+	IBOutlet NSSlider * backgroundRedColor;
+	IBOutlet NSSlider * backgroundGreenColor;
+	IBOutlet NSSlider * backgroundBlueColor;
+	
+	IBOutlet NSSlider * generalNumberAlpha;
+	IBOutlet NSSlider * generalNumber1;	
+	IBOutlet NSSlider * generalNumber2;		
+	IBOutlet NSSlider * generalNumber3;	
+	IBOutlet NSSlider * generalNumber4;	
+	
+	IBOutlet NSSlider * noiseAlpha;
+	IBOutlet NSSegmentedControl * noiseBlending;
+	IBOutlet NSColorWell * noiseColor1;
+	IBOutlet NSColorWell * noiseColor2;
+	
 	float r,g,b;
 	float r2,g2,b2;
 	float master;
@@ -66,7 +100,7 @@
 	
 	NSColor * color;
 	
-	IBOutlet NSColorWell * backgroundColor;
+	
 	IBOutlet NSButton * backgroundGradient;
 	IBOutlet NSSlider * backgroundGradientSpeed;
 	IBOutlet NSSlider * backgroundGradientRotation;
@@ -82,7 +116,15 @@
 }
 
 -(void) updateDmx:(id)param;
--(void) makeNumber:(int)n r:(float)_r g:(float)_g b:(float)_b a:(float)_a;
--(LedLamp*) getLamp:(int)x y:(int)y;
+-(void) makeNumber:(int)n intoArray:(bool*) array;
+
+-(IBAction) setBackgroundRed:(id)sender;
+-(IBAction) setBackgroundGreen:(id)sender;
+-(IBAction) setBackgroundBlue:(id)sender;
+-(IBAction) setBackground:(id)sender;
+
+
+
+//-(LedLamp*) getLamp:(int)x y:(int)y;
 
 @end
