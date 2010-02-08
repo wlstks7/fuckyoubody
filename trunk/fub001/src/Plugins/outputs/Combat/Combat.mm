848c7e0b58b64cb3ba2a33a45364a4a46b5a8085
jonas@halfdanj.dk <jonas@halfdanj.dk@e8aa6d0e-c866-11de-ba9a-9db95b2bc6c5>
2010-02-08 16:28:44 +0000

    Lemmings explode and die like they should

--- trunk/fub001/src/Plugins/outputs/Combat/Combat.mm
+++ trunk/fub001/src/Plugins/outputs/Combat/Combat.mm
@@ -18,51 +18,51 @@
 	
 	rot += ([rotation floatValue] -rot)*0.015;
 	
-	[GetPlugin(ProjectionSurfaces) apply:"Back" surface:"Floor"];
-	glPushMatrix();
-	glTranslated(0.5, 0.5, 0);
-	glRotated(rot, 0, 0, 1);
-	for(int i=0;i<4;i++){
-		glRotated(90, 0, 0, 1);
-		NSColor * c;
-		if(i==0)
-			c = [GetPlugin(Players) playerColor:1];
-		if(i==1)
-			c = [GetPlugin(Players) playerColor:3];
-		if(i==2)
-			c = [GetPlugin(Players) playerColor:4];
-		if(i==3)
-			c = [GetPlugin(Players) playerColor:2];
-		ofSetColor([c redComponent]*255, [c greenComponent]*255, [c blueComponent]*255);
-		
-		ofTriangle(0, 0,
-				   1.0, -1.0,
-				   1.0, 1.0);
-	}
-	glPopMatrix();
-	glPopMatrix();
+	[GetPlugin(ProjectionSurfaces) apply:"Back" surface:"Floor"];{
+		glPushMatrix();{
+			glTranslated(0.5, 0.5, 0);
+			glRotated(rot, 0, 0, 1);
+			for(int i=0;i<4;i++){
+				glRotated(90, 0, 0, 1);
+				NSColor * c;
+				if(i==0)
+					c = [GetPlugin(Players) playerColor:1];
+				if(i==1)
+					c = [GetPlugin(Players) playerColor:3];
+				if(i==2)
+					c = [GetPlugin(Players) playerColor:4];
+				if(i==3)
+					c = [GetPlugin(Players) playerColor:2];
+				ofSetColor([c redComponent]*255, [c greenComponent]*255, [c blueComponent]*255);
+				
+				ofTriangle(0, 0,
+						   1.0, -1.0,
+						   1.0, 1.0);
+			}
+		}glPopMatrix();
+	}glPopMatrix();
 	/*[GetPlugin(ProjectionSurfaces) apply:"Front" surface:"Floor"];
-	glPushMatrix();
-	glTranslated(0.5, 0.5, 0);
-	glRotated([rotation floatValue], 0, 0, 1);
-	for(int i=0;i<4;i++){
-		glRotated(90, 0, 0, 1);
-		NSColor * c;
-		if(i==0)
-			c = [GetPlugin(Players) playerColor:1];
-		if(i==1)
-			c = [GetPlugin(Players) playerColor:3];
-		if(i==2)
-			c = [GetPlugin(Players) playerColor:4];
-		if(i==3)
-			c = [GetPlugin(Players) playerColor:2];
-		ofSetColor([c redComponent]*255, [c greenComponent]*255, [c blueComponent]*255);
-		
-		ofTriangle(0, 0,
-				   1.0, -1.0,
-				   1.0, 1.0);
-	}
-	glPopMatrix();
-	glPopMatrix();*/
+	 glPushMatrix();
+	 glTranslated(0.5, 0.5, 0);
+	 glRotated([rotation floatValue], 0, 0, 1);
+	 for(int i=0;i<4;i++){
+	 glRotated(90, 0, 0, 1);
+	 NSColor * c;
+	 if(i==0)
+	 c = [GetPlugin(Players) playerColor:1];
+	 if(i==1)
+	 c = [GetPlugin(Players) playerColor:3];
+	 if(i==2)
+	 c = [GetPlugin(Players) playerColor:4];
+	 if(i==3)
+	 c = [GetPlugin(Players) playerColor:2];
+	 ofSetColor([c redComponent]*255, [c greenComponent]*255, [c blueComponent]*255);
+	 
+	 ofTriangle(0, 0,
+	 1.0, -1.0,
+	 1.0, 1.0);
+	 }
+	 glPopMatrix();
+	 glPopMatrix();*/
 }
 @end
